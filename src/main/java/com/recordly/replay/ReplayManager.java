0f130cd2f7f10e94ca0f88f92a7ce1af8146378e
webyep-art <197855412+webyep-art@users.noreply.github.com>
2026-09-04 03:34:48 +0300

    Provide ServerLinks.EMPTY and ConnectionType to cookie to fix PauseScreen NPE

--- src/main/java/com/recordly/replay/ReplayManager.java
+++ src/main/java/com/recordly/replay/ReplayManager.java
@@ -29,6 +29,7 @@ import net.minecraft.network.protocol.game.GameProtocols;
 import net.minecraft.resources.RegistryDataLoader;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.RegistryLayer;
+import net.minecraft.server.ServerLinks;
 import net.minecraft.server.WorldLoader;
 import net.minecraft.server.packs.repository.PackRepository;
 import net.minecraft.server.packs.resources.CloseableResourceManager;
@@ -156,7 +157,8 @@ public class ReplayManager {
                     null,
                     false,
                     Collections.emptyMap(),
-                    null
+                    ServerLinks.EMPTY,
+                    ConnectionType.NEOFORGE
             );
 
             this.packetListener = new ClientPacketListener(mc, connection, cookie);
